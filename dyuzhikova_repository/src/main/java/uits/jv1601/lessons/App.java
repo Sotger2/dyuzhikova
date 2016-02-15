d34c3047cee640108d903c6f2e0aedf6bba633af
Sotger2 <sokollovushka@mail.ru>
2016-02-15 21:37:32 +0200

    first commit

---
+++ dyuzhikova_repository/src/main/java/uits/jv1601/lessons/App.java
@@ -0,0 +1,13 @@
+package uits.jv1601.lessons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
